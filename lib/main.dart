5b37e65f82dd074e73a5784b2284ed3f35f49c7c
Plasius <atiiworks@gmail.com>
2022-03-07 21:54:40 -0500

    firebase intergration

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
-import 'package:firebase_core/firebase_core.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
+  /*await Firebase.initializeApp(
     //TODO: add Firebase project with command $ flutterfire configure
     options: DefaultFirebaseOptions.currentPlatform,
-  );
+  );*/
   runApp(const MyApp());
 }
 
