1976899d2cd508a0b6e9cd7f3e366546c34fa73c
Plasius <Plasius@users.noreply.github.com>
2022-04-11 17:29:38 -0400

    Replacing feed page with chat page for tinder

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -8,13 +9,143 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection('tinder/userid1_userid2/messages');
+  var alert = '';
+  bool isAdmin = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         resizeToAvoidBottomInset: false,
         appBar: AppBar(
-          title: const Text('messaged_user'),
+          title: const Text('Feed'),
         ),
-        body: const Text("welcome"));
+        body: SingleChildScrollView(
+            child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection('tinder/userid1_userid2/messages')
+              .snapshots(includeMetadataChanges: true),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('Loading');
+            } else {
+              return Column(children: [
+                ListView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  reverse: true,
+                  children: snapshot.data!.docs.map((document) {
+                    return Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Column(children: [
+                          Text(
+                            document['id'],
+                            style: const TextStyle(
+                                color: Colors.black, fontSize: 22),
+                          ),
+                          Text(document['message'],
+                              style: const TextStyle(
+                                  color: Colors.black, fontSize: 22))
+                        ]));
+                  }).toList(),
+                ),
+                if (isAdmin)
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('Country List'),
+                                    content: setupAlertDialoadContainer(),
+                                  );
+                                });
+                          },
+                          iconSize: 50,
+                          icon: const Icon(Icons.question_mark)),
+                      IconButton(
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('Country List'),
+                                    content: setupAlertDialoadContainer(),
+                                  );
+                                });
+                          },
+                          iconSize: 50,
+                          icon: const Icon(Icons.circle)),
+                      IconButton(
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('Country List'),
+                                    content: setupAlertDialoadContainer(),
+                                  );
+                                });
+                          },
+                          iconSize: 50,
+                          icon: const Icon(Icons.location_pin)),
+                      IconButton(
+                          onPressed: () {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: const Text('Country List'),
+                                    content: setupAlertDialoadContainer(),
+                                  );
+                                });
+                          },
+                          iconSize: 50,
+                          icon: const Icon(Icons.timer))
+                    ],
+                  )
+              ]);
+            }
+          },
+        )));
+  }
+
+  Widget setupAlertDialoadContainer() {
+    return SizedBox(
+      height: 300.0, // Change as per your requirement
+      width: 300.0, // Change as per your requirement
+      child: ListView.builder(
+        shrinkWrap: true,
+        itemCount: 5,
+        itemBuilder: (BuildContext context, int index) {
+          return GestureDetector(
+            child: ElevatedButton(
+              onPressed: () {
+                sendMyMessage(index);
+              },
+              child: const Text("click me"),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  sendMyMessage(int index) {
+    messages.add({
+      'id': "userid" + index.toString(),
+      'message': "message" + index.toString()
+    });
+    Navigator.pop(context);
   }
 }
