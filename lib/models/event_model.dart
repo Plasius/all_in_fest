ba0b0049ebe247f7f6752d03b6e2cd4f269bbeb1
GyureszBa <gyorgy.dezsenyi@gmail.com>
2022-04-19 11:09:28 +0200

    Created a model for the events

---
+++ lib/models/event_model.dart
@@ -0,0 +1,39 @@
+import 'package:json_annotation/json_annotation.dart';
+import 'package:cloud_firestore_odm/cloud_firestore_odm.dart';
+
+@JsonSerializable(explicitToJson: true)
+class Event {
+  String name;
+  String artists;
+  String imgUrl;
+  String place;
+  DateTime date;
+  String bio;
+
+  Event(
+      {required this.name,
+      required this.artists,
+      required this.imgUrl,
+      required this.place,
+      required this.date,
+      required this.bio});
+
+  factory Event.fromJson(Map<String, dynamic> json) {
+    return Event(
+        name: json['name'],
+        artists: json['artists'],
+        imgUrl: json['imgUrl'],
+        place: json['place'],
+        date: json['date'],
+        bio: json['bio']);
+  }
+
+  Map<String, dynamic> toJson() => {
+        'name': name,
+        'artists': artists,
+        'imgUrl': imgUrl,
+        'place': place,
+        'date': date,
+        'bio': bio
+      };
+}
