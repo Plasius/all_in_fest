ba21ce037fe0c57b67b7e8c65003432fa2186f11
Plasius <Plasius@users.noreply.github.com>
2022-05-17 15:17:48 -0400

    Loginpage

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'matches_page.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -10,16 +13,67 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    String email = "";
+    String password = "";
+
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: Text('Login World'),
+        body: Center(
+          child: Column(children: [
+            const Text("E-mail"),
+            TextFormField(onChanged: (value) {
+              email = value;
+            }),
+            const Text("Password"),
+            TextFormField(
+              onChanged: (value) {
+                password = value;
+              },
+            ),
+            ElevatedButton(
+              child: const Text('Submit'),
+              onPressed: () {
+                try {
+                  signInUsingEmailPassword(
+                      email: email, password: password, context: context);
+                } catch (e) {
+                  print("whoops");
+                }
+              },
+            ),
+          ]),
         ),
       ),
     );
+  }
+
+  static Future<User?> signInUsingEmailPassword({
+    required String email,
+    required String password,
+    required BuildContext context,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User? user;
+
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided.');
+      }
+    }
+
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const MatchesPage()));
   }
 }
