4dee95190f673e047e586483d33f5853c51f7b3a
GyureszBa <gyorgy.dezsenyi@gmail.com>
2022-05-18 16:45:37 +0200

    Detail page almost finished

--- lib/pages/event_details.dart
+++ lib/pages/event_details.dart
@@ -38,9 +38,7 @@ class _DetailPageState extends State<DetailPage> {
         ],
       ),
       body: Container(
-        decoration: BoxDecoration(
-          color: Color.fromRGBO(97, 42, 122, 1)
-        ),
+        decoration: BoxDecoration(color: Color.fromRGBO(97, 42, 122, 1)),
         child: Column(
           children: [
             Padding(
@@ -53,27 +51,35 @@ class _DetailPageState extends State<DetailPage> {
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text("Fellépő neve", style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold
-                      ),),
+                      Text(
+                        "Fellépő neve",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
                       Row(
                         children: [
-                          Text("Nap", style: TextStyle(
-                            color: Colors.white
-                          ),),
-                          Text("9:50-11:20", style: TextStyle(
-                            color: Colors.white
-                          ),)
+                          Text(
+                            "Nap",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          Text(
+                            "9:50-11:20",
+                            style: TextStyle(color: Colors.white),
+                          )
                         ],
                       ),
-                      Text("Helyszín", style: TextStyle(
-                        color: Colors.white
-                      ),)
+                      Text(
+                        "Helyszín",
+                        style: TextStyle(color: Colors.white),
+                      )
                     ],
                   ),
                   Padding(
                     padding: const EdgeInsets.only(right: 30.0),
-                    child: Icon(Icons.favorite_border_sharp, color: Colors.white,),
+                    child: Icon(
+                      Icons.favorite_border_sharp,
+                      color: Colors.white,
+                    ),
                   )
                 ],
               ),
@@ -82,12 +88,11 @@ class _DetailPageState extends State<DetailPage> {
               child: Padding(
                 padding: const EdgeInsets.all(30.0),
                 child: Container(
-                  height: MediaQuery.of(context).size.height/3,
-                  decoration: BoxDecoration(
-                    color: Colors.white
-                  ),
+                  height: MediaQuery.of(context).size.height / 3,
+                  decoration: BoxDecoration(color: Colors.white),
                   child: Padding(
-                    padding: const EdgeInsets.only(left: 10, top: 10, right: 10, bottom: 20),
+                    padding: const EdgeInsets.only(
+                        left: 10, top: 10, right: 10, bottom: 20),
                     child: Image(
                       image: AssetImage("lib/assets/images/artist.png"),
                       fit: BoxFit.cover,
@@ -95,6 +100,13 @@ class _DetailPageState extends State<DetailPage> {
                   ),
                 ),
               ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed augue consectetur, dapibus neque sit amet, dictum mi. Mauris quis ex ac augue gravida pretium. Aliquam posuere eu odio pellentesque pellentesque. Vivamus interdum erat ac mauris consectetur, eu pharetra neque suscipit. Nunc id mi eu augue elementum condimentum ac non mauris. Nullam suscipit lacus et gravida gravida. Quisque condimentum vitae neque non fermentum. Vivamus ultricies vestibulum mi sit amet convallis. Aliquam nec arcu metus. Integer semper gravida odio, ut semper ipsum dapibus sed. Morbi ac ex eget quam iaculis congue. Maecenas laoreet dolor vel risus lacinia fermentum porttitor vel dolor. Quisque sit amet nunc hendrerit nunc luctus varius. In ac scelerisque arcu. Etiam at dignissim elit.", style: TextStyle(
+                color: Colors.white
+              ),),
             )
           ],
         ),
