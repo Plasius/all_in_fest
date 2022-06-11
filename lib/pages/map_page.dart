0c9c11deb2c2765798ad277e036aed07014941fe
Zeke Gábor <zgabor2001@gmail.com>
2022-06-11 21:58:06 +0200

    map basics

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MapPage extends StatefulWidget {
@@ -13,22 +14,48 @@ class _MapPageState extends State<MapPage> {
     return MaterialApp(
       title: 'Map',
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(232, 107, 62, 1),
-          leading: const Icon(
-            Icons.menu,
-            color: Colors.white,
+          appBar: AppBar(
+            backgroundColor: const Color.fromRGBO(232, 107, 62, 1),
+            leading: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+            title: const Image(
+              image: AssetImage("lib/assets/images/logo.png"),
+              height: 50,
+              fit: BoxFit.contain,
+            ),
           ),
-          title: const Image(
-            image: AssetImage("lib/assets/images/logo.png"),
-            height: 50,
-            fit: BoxFit.contain,
-          ),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+          body: 
+          SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topLeft,
+                    child: const Text(
+                      'Térkép',
+                      /*textAlign: TextAlign.left,
+                      textDirection: TextDirection.rtl,*/
+                      style: TextStyle(
+                          fontFamily: 'Jost',
+                          //fontStyle: FontStyle.italic,
+                          fontSize: 42),
+                    ),
+                  ),
+                  const Text(
+                    'Térkép alcím',
+                    style: TextStyle(
+                        fontFamily: 'Jost',
+                        fontStyle: FontStyle.italic,
+                        fontSize: 22),
+                  ),
+                  Image.asset("lib/assets/images/angry-annoyed.gif",
+                      height: 500, width: 500)
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
