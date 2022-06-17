7de0abc72c0badab781f9f631fa1566eca7a371a
GyureszBa <gyorgy.dezsenyi@gmail.com>
2022-06-17 16:03:28 +0200

    image upload functions

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:date_field/date_field.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
@@ -13,7 +18,9 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   DateTime selectedDate = DateTime.now();
-  var photoURL;
+  var photoURL = "";
+  File? _photo;
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
@@ -36,10 +43,10 @@ class _ProfilePageState extends State<ProfilePage> {
                 fit: BoxFit.contain,
               ),
             ),
-            body: editBody()));
+            body: editBody(context)));
   }
 
-  Widget editBody() {
+  Widget editBody(BuildContext context) {
     return SingleChildScrollView(
       child: Padding(
         padding: const EdgeInsets.all(15),
@@ -48,17 +55,16 @@ class _ProfilePageState extends State<ProfilePage> {
             Padding(
               padding: const EdgeInsets.only(top: 15),
               child: Center(
-                child: Stack(
-                  children: [
-                    GestureDetector(
-                      onTap: () => {uploadPhoto()},
-                      child: Container(
+                child: GestureDetector(
+                  onTap: () => _showPicker(context),
+                  child: Stack(
+                    children: [
+                      Container(
                         width: 130,
                         height: 130,
                         decoration: BoxDecoration(
                             shape: BoxShape.circle,
-                            image:
-                                DecorationImage(image: NetworkImage(photoURL)),
+                            image: DecorationImage(image: NetworkImage(photoURL)),
                             boxShadow: [
                               BoxShadow(
                                   spreadRadius: 2,
@@ -67,22 +73,22 @@ class _ProfilePageState extends State<ProfilePage> {
                                   color: Colors.grey.withOpacity(0.3))
                             ]),
                       ),
-                    ),
-                    Positioned(
-                      child: Container(
-                        height: 40,
-                        width: 40,
-                        decoration: BoxDecoration(
-                            color: Colors.redAccent, shape: BoxShape.circle),
-                        child: Icon(
-                          MdiIcons.cameraPlus,
-                          color: Colors.white,
+                      Positioned(
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              color: Colors.redAccent, shape: BoxShape.circle),
+                          child: Icon(
+                            MdiIcons.cameraPlus,
+                            color: Colors.white,
+                          ),
                         ),
-                      ),
-                      bottom: 0,
-                      right: 0,
-                    )
-                  ],
+                        bottom: 0,
+                        right: 0,
+                      )
+                    ],
+                  ),
                 ),
               ),
             ),
@@ -172,10 +178,6 @@ class _ProfilePageState extends State<ProfilePage> {
     );
   }
 
-  uploadPhoto() {
-    //logic to select photo and upload to firestorage
-  }
-
   void loadImage() async {
     final storageRef =
         FirebaseStorage.instanceFor(bucket: "gs://festival-2e218.appspot.com")
@@ -183,6 +185,75 @@ class _ProfilePageState extends State<ProfilePage> {
     final pathReference = storageRef.child('testimage.png');
 
     photoURL = await pathReference.getDownloadURL();
-    build(context);
+    setState(() {});
+  }
+
+  Future imgFromGallery() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    setState(() {
+      if (pickedFile != null) {
+        _photo = File(pickedFile.path);
+        uploadFile();
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future imgFromCamera() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      if (pickedFile != null) {
+        _photo = File(pickedFile.path);
+        uploadFile();
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
+  Future uploadFile() async {
+    if (_photo == null) return;
+    final fileName = basename(_photo!.path);
+    final destination = 'files/$fileName';
+
+    try {
+      final ref = FirebaseStorage.instance.ref(destination).child('file/');
+      await ref.putFile(_photo!);
+    } catch (e) {
+      print('error occured');
+    }
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return SafeArea(
+            child: Container(
+              child: new Wrap(
+                children: <Widget>[
+                  new ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Gallery'),
+                      onTap: () {
+                        imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  new ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Camera'),
+                    onTap: () {
+                      imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
--- lib/pages/swipe_page.dart
+++ lib/pages/swipe_page.dart
@@ -1,6 +1,6 @@
 import 'package:all_in_fest/pages/matches_page.dart';
 import 'package:flutter/material.dart';
-
+import 'package:swipe_cards/swipe_cards.dart';
 
 class SwipePage extends StatefulWidget {
   const SwipePage({Key? key}) : super(key: key);
@@ -10,35 +10,63 @@ class SwipePage extends StatefulWidget {
 }
 
 class _SwipePageState extends State<SwipePage> {
+  MatchEngine? _matchEngine;
+  List<SwipeItem> _swipeItems = <SwipeItem>[];
+  List<String> _profiles = [];
+  void initState() {
+    for (int i = 0; i<_profiles.length; i++){
+      _swipeItems.add(SwipeItem(
+        content: "",
+        likeAction: () => messageOptions(),
+        nopeAction: () => showNopeGif(),
+        superlikeAction: () => showHornyGif()
+      ));
+    }
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(232, 107, 62, 1),
-          leading: const Icon(
-            Icons.menu,
-            color: Colors.white,
+          appBar: AppBar(
+            backgroundColor: const Color.fromRGBO(232, 107, 62, 1),
+            leading: const Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+            title: const Image(
+              image: const AssetImage("lib/assets/images/logo.png"),
+              height: 50,
+              fit: BoxFit.contain,
+            ),
           ),
-          title: const Image(
-            image: const AssetImage("lib/assets/images/logo.png"),
-            height: 50,
-            fit: BoxFit.contain,
-          ),
-        ),
-        body: swipeBody()
-      ),
+          body: swipeBody()),
     );
   }
+
   Widget swipeBody() {
     var size = MediaQuery.of(context).size;
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Container(
         height: size.height,
+        child: SwipeCards(
+          matchEngine: _matchEngine!,
+          itemBuilder: (BuildContext context, int index) {
+            return Container();
+          },
+          onStackFinished: () => showHornyGif(),
+        ),
       ),
     );
   }
 
+  void messageOptions() {}
+
+  void showNopeGif() {}
+
+  void showHornyGif() {}
 }
