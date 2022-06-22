65bf4f3d74426abe3b628d4361a5286136a3d08d
Plasius <Plasius@users.noreply.github.com>
2022-06-22 17:00:35 +0200

    Update swipe_page.dart

--- lib/pages/swipe_page.dart
+++ lib/pages/swipe_page.dart
@@ -1,5 +1,7 @@
 import 'package:all_in_fest/pages/matches_page.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:swipe_cards/swipe_cards.dart';
 
 class SwipePage extends StatefulWidget {
@@ -12,39 +14,84 @@ class SwipePage extends StatefulWidget {
 class _SwipePageState extends State<SwipePage> {
   MatchEngine? _matchEngine;
   List<SwipeItem> _swipeItems = <SwipeItem>[];
-  List<String> _profiles = [];
-  void initState() {
-    for (int i = 0; i<_profiles.length; i++){
+  List<DocumentSnapshot> _profiles = [];
+
+  void getProfiles() async {
+    //final prefs = await SharedPreferences.getInstance();
+
+    //int? counter = prefs.getInt('counter');
+
+    FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
+    await firebaseFirestore
+        .collection('users')
+        //.orderBy('since')
+        //.startAfter([counter])
+        //.limit(2)
+        .get()
+        .then((QuerySnapshot querySnapshot) => {
+              print('running' + querySnapshot.docs[0].toString()),
+              querySnapshot.docs.forEach((doc) {
+                _profiles.add(doc);
+                print('added');
+              })
+            });
+
+    print(_profiles.length);
+
+    for (int i = 0; i < _profiles.length; i++) {
       _swipeItems.add(SwipeItem(
-        content: "",
-        likeAction: () => messageOptions(),
-        nopeAction: () => showNopeGif(),
-        superlikeAction: () => showHornyGif()
-      ));
+          content: NetworkImage(_profiles[i]['photo']),
+          likeAction: () => messageOptions(),
+          nopeAction: () => showNopeGif(),
+          superlikeAction: () => showHornyGif()));
     }
 
+    setState(() {});
+  }
+
+  void initState() {
     super.initState();
+    _matchEngine = new MatchEngine(swipeItems: _swipeItems);
+    getProfiles();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: const Color.fromRGBO(232, 107, 62, 1),
-            leading: const Icon(
-              Icons.menu,
-              color: Colors.white,
+    if (_profiles.length != 0)
+      return MaterialApp(
+        title: 'Welcome to Flutter',
+        home: Scaffold(
+            appBar: AppBar(
+              backgroundColor: const Color.fromRGBO(232, 107, 62, 1),
+              leading: const Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+              title: const Image(
+                image: const AssetImage("lib/assets/images/logo.png"),
+                height: 50,
+                fit: BoxFit.contain,
+              ),
             ),
-            title: const Image(
-              image: const AssetImage("lib/assets/images/logo.png"),
-              height: 50,
-              fit: BoxFit.contain,
-            ),
-          ),
-          body: swipeBody()),
-    );
+            body: swipeBody()),
+      );
+    else
+      return MaterialApp(
+          title: 'Welcome to Flutter',
+          home: Scaffold(
+              appBar: AppBar(
+                backgroundColor: const Color.fromRGBO(232, 107, 62, 1),
+                leading: const Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+                title: const Image(
+                  image: const AssetImage("lib/assets/images/logo.png"),
+                  height: 50,
+                  fit: BoxFit.contain,
+                ),
+              ),
+              body: CircularProgressIndicator()));
   }
 
   Widget swipeBody() {
@@ -56,7 +103,12 @@ class _SwipePageState extends State<SwipePage> {
         child: SwipeCards(
           matchEngine: _matchEngine!,
           itemBuilder: (BuildContext context, int index) {
-            return Container();
+            return Center(
+              child: Container(
+                  decoration: BoxDecoration(
+                      image:
+                          DecorationImage(image: _swipeItems[index].content))),
+            );
           },
           onStackFinished: () => showHornyGif(),
         ),
