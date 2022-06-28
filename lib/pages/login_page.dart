447c115574d64a0f7993beb761e918fce6b2dc7a
GyureszBa <gyorgy.dezsenyi@gmail.com>
2022-06-28 13:49:56 +0200

    Login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -19,42 +19,72 @@ class _LoginPageState extends State<LoginPage> {
     return MaterialApp(
       title: 'Login',
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: const Color.fromRGBO(232, 107, 62, 1),
-          leading: const Icon(
-            Icons.menu,
-            color: Colors.white,
-          ),
-          title: const Image(
-            image: AssetImage("lib/assets/images/logo.png"),
-            height: 50,
-            fit: BoxFit.contain,
-          ),
-        ),
-        body: Center(
-          child: Column(children: [
-            const Text("E-mail"),
-            TextFormField(onChanged: (value) {
-              email = value;
-            }),
-            const Text("Password"),
-            TextFormField(
-              onChanged: (value) {
-                password = value;
-              },
+        body: Container(
+          constraints: BoxConstraints.expand(),
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("lib/assets/images/login_background.png"),
+                  fit: BoxFit.cover)),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  left: 8.0, right: 8, top: 100, bottom: 0),
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Color.fromRGBO(97, 42, 122, 1),
+                    borderRadius: BorderRadius.circular(5)),
+                height: MediaQuery.of(context).size.height * 0.52,
+                width: MediaQuery.of(context).size.width,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 10, top: 24),
+                        child: Center(
+                          child: const Text(
+                            "Login",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 36,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                      const Text("E-mail"),
+                      TextFormField(
+                        onChanged: (value) {
+                          email = value;
+                        },
+                        decoration: InputDecoration(
+                            fillColor: Color.fromRGBO(255, 255, 255, 0.4),
+                            border: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(color: Colors.white, width: 3))),
+                      ),
+                      const Text("Password"),
+                      TextFormField(
+                        onChanged: (value) {
+                          password = value;
+                        },
+                      ),
+                      ElevatedButton(
+                        child: const Text('Submit'),
+                        onPressed: () {
+                          try {
+                            signInUsingEmailPassword(
+                                email: email,
+                                password: password,
+                                context: context);
+                          } catch (e) {
+                            print("whoops");
+                          }
+                        },
+                      ),
+                    ]),
+              ),
             ),
-            ElevatedButton(
-              child: const Text('Submit'),
-              onPressed: () {
-                try {
-                  signInUsingEmailPassword(
-                      email: email, password: password, context: context);
-                } catch (e) {
-                  print("whoops");
-                }
-              },
-            ),
-          ]),
+          ),
         ),
       ),
     );
