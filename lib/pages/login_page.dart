2be5d5c7d33bcf6fdbdc6142e8b498df7dc82e41
GyureszBa <gyorgy.dezsenyi@gmail.com>
2022-07-05 22:06:31 +0200

    login page bug fix

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -46,121 +46,123 @@ class _LoginPageState extends State<LoginPage> {
                         borderRadius: BorderRadius.circular(5)),
                     height: MediaQuery.of(context).size.height * 0.52,
                     width: MediaQuery.of(context).size.width,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 24, bottom: 20),
-                            child: Center(
-                              child: const Text(
-                                "Bejelentkezés",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 30,
-                                    fontWeight: FontWeight.bold),
-                              ),
-                            ),
-                          ),
-                          Padding(
-                            padding:
-                                const EdgeInsets.only(left: 16.0, bottom: 10),
-                            child: const Text(
-                              "E-mail cím",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.normal),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                left: 16, right: 16, bottom: 32),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Color.fromRGBO(255, 255, 255, 0.4),
-                                  border:
-                                      Border.all(color: Colors.white, width: 3),
-                                  borderRadius: BorderRadius.circular(5)),
-                              child: TextFormField(
-                                onChanged: (value) => email = value,
-                                decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  prefixIcon: Icon(
-                                    Icons.mail_outlined,
-                                    color: Colors.white,
-                                  ),
+                    child: SingleChildScrollView(
+                      child: Column(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.only(top: 24, bottom: 20),
+                              child: Center(
+                                child: const Text(
+                                  "Bejelentkezés",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.bold),
                                 ),
                               ),
                             ),
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(left: 16, bottom: 10),
-                            child: const Text(
-                              "Jelszó",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.normal),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 16.0, bottom: 10),
+                              child: const Text(
+                                "E-mail cím",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.normal),
+                              ),
                             ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(
-                                left: 16, right: 16, bottom: 48),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                  color: Color.fromRGBO(255, 255, 255, 0.4),
-                                  border:
-                                      Border.all(color: Colors.white, width: 3),
-                                  borderRadius: BorderRadius.circular(5)),
-                              child: TextFormField(
-                                  onChanged: (value) => password = value,
-                                  obscureText: _showPassword ? false : true,
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 16, right: 16, bottom: 32),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Color.fromRGBO(255, 255, 255, 0.4),
+                                    border:
+                                        Border.all(color: Colors.white, width: 3),
+                                    borderRadius: BorderRadius.circular(5)),
+                                child: TextFormField(
+                                  onChanged: (value) => email = value,
                                   decoration: InputDecoration(
                                     border: InputBorder.none,
                                     prefixIcon: Icon(
-                                      Icons.lock_outline,
+                                      Icons.mail_outlined,
                                       color: Colors.white,
                                     ),
-                                    suffixIcon: GestureDetector(
-                                      onTap: () {
-                                        _togglevisibility();
-                                      },
-                                      child: Icon(
-                                        _showPassword
-                                            ? Icons.visibility
-                                            : Icons.visibility_off,
-                                        color: Colors.white,
-                                      ),
-                                    ),
-                                  )),
-                            ),
-                          ),
-                          Center(
-                            child: GestureDetector(
-                              onTap: () => signInUsingEmailPassword(
-                                  context: context,
-                                  email: email,
-                                  password: password),
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    color: Color.fromRGBO(254, 192, 1, 1),
-                                    borderRadius: BorderRadius.circular(5)),
-                                child: Padding(
-                                  padding: const EdgeInsets.symmetric(
-                                      vertical: 8.0, horizontal: 48),
-                                  child: const Text(
-                                    "Belépés",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 32),
                                   ),
                                 ),
                               ),
                             ),
-                          )
-                        ]),
+                            Padding(
+                              padding: EdgeInsets.only(left: 16, bottom: 10),
+                              child: const Text(
+                                "Jelszó",
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.normal),
+                              ),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(
+                                  left: 16, right: 16, bottom: 48),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                    color: Color.fromRGBO(255, 255, 255, 0.4),
+                                    border:
+                                        Border.all(color: Colors.white, width: 3),
+                                    borderRadius: BorderRadius.circular(5)),
+                                child: TextFormField(
+                                    onChanged: (value) => password = value,
+                                    obscureText: _showPassword ? false : true,
+                                    decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      prefixIcon: Icon(
+                                        Icons.lock_outline,
+                                        color: Colors.white,
+                                      ),
+                                      suffixIcon: GestureDetector(
+                                        onTap: () {
+                                          _togglevisibility();
+                                        },
+                                        child: Icon(
+                                          _showPassword
+                                              ? Icons.visibility
+                                              : Icons.visibility_off,
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    )),
+                              ),
+                            ),
+                            Center(
+                              child: GestureDetector(
+                                onTap: () => signInUsingEmailPassword(
+                                    context: context,
+                                    email: email,
+                                    password: password),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Color.fromRGBO(254, 192, 1, 1),
+                                      borderRadius: BorderRadius.circular(5)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 8.0, horizontal: 48),
+                                    child: const Text(
+                                      "Belépés",
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 32),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            )
+                          ]),
+                    ),
                   ),
                 ),
               ),
