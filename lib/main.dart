3ed5d2af6618b97300492500d7d8a99a15e964d9
Plasius <Plasius@users.noreply.github.com>
2022-08-02 22:07:09 +0300

    Home screen with debug buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,15 @@
 import 'package:all_in_fest/pages/events_page.dart';
-import 'package:all_in_fest/pages/matches_page.dart';
 import 'package:all_in_fest/pages/login_page.dart';
+import 'package:all_in_fest/pages/map_page.dart';
 import 'package:all_in_fest/pages/profile_page.dart';
 import 'package:all_in_fest/pages/register_page.dart';
+import 'package:all_in_fest/pages/settings_page.dart';
 import 'package:all_in_fest/pages/swipe_page.dart';
 import 'package:provider/provider.dart';
 import 'package:realm/realm.dart';
 import 'package:flutter/material.dart';
 
 import 'models/favorite_model.dart';
-import 'pages/map_page.dart';
-import 'pages/settings_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -71,7 +70,316 @@ class _MyHomePageState extends State<MyHomePage> {
             fit: BoxFit.contain,
           ),
         ),
-        body: Center(
+        body: Container(
+          constraints: const BoxConstraints.expand(),
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("lib/assets/home_bg.png"),
+                  fit: BoxFit.cover)),
+          child: SingleChildScrollView(
+            child: Row(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(left: 20),
+                  child: Column(
+                    children: [
+                      GestureDetector(
+                        onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginPage())),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(20)),
+                              color: const Color.fromRGBO(97, 42, 122, .40),
+                              border:
+                                  Border.all(color: Colors.white, width: 3)),
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: MediaQuery.of(context).size.height * 0.21,
+                          child: Column(
+                            children: [
+                              Icon(Icons.developer_board,
+                                  color: Colors.white,
+                                  size:
+                                      MediaQuery.of(context).size.width * 0.3),
+                              Text(
+                                "LOGIN",
+                                style: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height *
+                                            0.02,
+                                    color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 1,
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        onTap: () => Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const EventsPage())),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(20)),
+                              color: const Color.fromRGBO(97, 42, 122, .40),
+                              border:
+                                  Border.all(color: Colors.white, width: 3)),
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: MediaQuery.of(context).size.height * 0.21,
+                          alignment: Alignment.center,
+                          child: Column(
+                            children: [
+                              Icon(Icons.calendar_month,
+                                  color: Colors.white,
+                                  size:
+                                      MediaQuery.of(context).size.width * 0.3),
+                              Text(
+                                "ESEMÉNYEK",
+                                style: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height *
+                                            0.02,
+                                    color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 1,
+                        height: 20,
+                      ),
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        child: const Image(
+                            image: AssetImage("lib/assets/leanykak.png")),
+                      ),
+                      const SizedBox(
+                        width: 1,
+                        height: 20,
+                      ),
+                      GestureDetector(
+                        child: Container(
+                          decoration: BoxDecoration(
+                              borderRadius:
+                                  const BorderRadius.all(Radius.circular(20)),
+                              color: const Color.fromRGBO(97, 42, 122, .40),
+                              border:
+                                  Border.all(color: Colors.white, width: 3)),
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: MediaQuery.of(context).size.height * 0.21,
+                          child: Column(
+                            children: [
+                              Icon(Icons.food_bank,
+                                  color: Colors.white,
+                                  size:
+                                      MediaQuery.of(context).size.width * 0.3),
+                              Text(
+                                "RENDELÉS",
+                                style: TextStyle(
+                                    fontSize:
+                                        MediaQuery.of(context).size.height *
+                                            0.02,
+                                    color: Colors.white),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.1,
+                  height: 1,
+                ),
+                Column(
+                  children: [
+                    GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const RegisterPage())),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            color: const Color.fromRGBO(97, 42, 122, .40),
+                            border: Border.all(color: Colors.white, width: 3)),
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.21,
+                        child: Column(
+                          children: [
+                            Icon(Icons.developer_board,
+                                color: Colors.white,
+                                size: MediaQuery.of(context).size.width * 0.3),
+                            Text(
+                              "REGISTER",
+                              style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                  color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 1,
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const ProfilePage())),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            color: const Color.fromRGBO(97, 42, 122, .40),
+                            border: Border.all(color: Colors.white, width: 3)),
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.21,
+                        child: Column(
+                          children: [
+                            Icon(Icons.developer_board,
+                                color: Colors.white,
+                                size: MediaQuery.of(context).size.width * 0.3),
+                            Text(
+                              "PROFILE",
+                              style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                  color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 1,
+                      height: 20,
+                    ),
+                    const Text("HOME",
+                        style: TextStyle(
+                            fontSize: 36,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white)),
+                    const SizedBox(
+                      width: 1,
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SwipePage())),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            color: const Color.fromRGBO(97, 42, 122, .40),
+                            border: Border.all(color: Colors.white, width: 3)),
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.21,
+                        child: Column(
+                          children: [
+                            Icon(Icons.people,
+                                color: Colors.white,
+                                size: MediaQuery.of(context).size.width * 0.3),
+                            Text(
+                              "MEETUP",
+                              style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                  color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 1,
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const MapPage())),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            color: const Color.fromRGBO(97, 42, 122, .40),
+                            border: Border.all(color: Colors.white, width: 3)),
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.21,
+                        child: Column(
+                          children: [
+                            Icon(Icons.map,
+                                color: Colors.white,
+                                size: MediaQuery.of(context).size.width * 0.3),
+                            Text(
+                              "TÉRKÉP",
+                              style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                  color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 1,
+                      height: 20,
+                    ),
+                    GestureDetector(
+                      onTap: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const SettingsPage())),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(20)),
+                            color: const Color.fromRGBO(97, 42, 122, .40),
+                            border: Border.all(color: Colors.white, width: 3)),
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        height: MediaQuery.of(context).size.height * 0.21,
+                        child: Column(
+                          children: [
+                            Icon(Icons.settings,
+                                color: Colors.white,
+                                size: MediaQuery.of(context).size.width * 0.3),
+                            Text(
+                              "BEÁLLÍTÁSOK",
+                              style: TextStyle(
+                                  fontSize:
+                                      MediaQuery.of(context).size.height * 0.02,
+                                  color: Colors.white),
+                            )
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        )
+
+        /*Center(
           child: Column(
             children: [
               GestureDetector(
@@ -196,6 +504,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ],
           ),
-        ));
+        )*/
+
+        );
   }
 }
